74710891948f86f22f06e78e2aa86ab9458f71a3
4eBelka <4eBelka@gmail.com>
2021-01-13 15:47:47 +0300

    Checking basket routing

---
+++ pages/basket_page.py
@@ -0,0 +1,13 @@
+from .base_page import BasePage
+from selenium.webdriver.common.by import By
+from .locators import BasketPageLocators
+import pytest
+
+class BasketPage(BasePage):
+    def checking_no_items_in_the_basket(self):
+        assert self.is_not_element_present(*BasketPageLocators.BASKET_BUY), \
+        "Button buy is presented, but should not be"
+        
+    def checking_message_basket_is_empty(self):
+        assert self.is_element_present(*BasketPageLocators.BASKET_MESSAGE_EMPTY), \
+        "No message indicating that the basket is empty"
--- pages/locators.py
+++ pages/locators.py
@@ -1,13 +1,21 @@
 from selenium.webdriver.common.by import By
 
-
+    
 class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
-    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc") 
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+    BUTTON_BASKET = (By.CSS_SELECTOR, ".pull-right  span a.btn.btn-default")
+
+class BasketPageLocators():
+    BASKET_BUY = (By.CSS_SELECTOR, ".btn-lg")
+    BASKET_MESSAGE_EMPTY = (By.CSS_SELECTOR, "#content_inner>p")
     
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+class MainPageLocators:
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     
 class ProductPageLocators():
     BUTTON_BASKET = (By.CSS_SELECTOR, ".btn-add-to-basket")
